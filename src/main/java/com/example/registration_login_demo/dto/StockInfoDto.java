751a5da97c33989035a361c437bd235735d2969a
kahchun-haha <kahchunlim885@gmail.com>
2023-06-10 05:47:11 +0800

    modified:   src/main/java/com/example/registration_login_demo/controller/StockController.java 	deleted:    src/main/java/com/example/registration_login_demo/dto/SearchDetailDto.java 	new file:   src/main/java/com/example/registration_login_demo/dto/StockInfoDto.java

---
+++ src/main/java/com/example/registration_login_demo/dto/StockInfoDto.java
@@ -0,0 +1,15 @@
+package com.example.registration_login_demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockInfoDto {
+    private String label;
+    private String value;
+}
