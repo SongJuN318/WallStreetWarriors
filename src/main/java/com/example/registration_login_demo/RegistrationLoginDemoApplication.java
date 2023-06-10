8fbcdc94d752529d279883344844dce04385e4ee
kahchun-haha <kahchunlim885@gmail.com>
2023-06-10 19:35:48 +0800

    modified:   src/main/java/com/example/registration_login_demo/RegistrationLoginDemoApplication.java 	new file:   src/main/java/com/example/registration_login_demo/service/NotificationService.java 	modified:   pom.xml 	modified:   src/main/resources/application.properties 	src/main/java/com/example/registration_login_demo/dto/NotificationDto.java

--- src/main/java/com/example/registration_login_demo/RegistrationLoginDemoApplication.java
+++ src/main/java/com/example/registration_login_demo/RegistrationLoginDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.registration_login_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RegistrationLoginDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/registration_login_demo/service/NotificationService.java
@@ -0,0 +1,37 @@
+package com.example.registration_login_demo.service;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.stereotype.Service;
+
+import com.example.registration_login_demo.dto.NotificationDto;
+
+@Service
+public class NotificationService {
+    private Session javaMailSender;
+
+    public NotificationService(Session javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendNotification(NotificationDto notification) {
+        MimeMessage mail = new MimeMessage(javaMailSender);
+
+        try {
+            mail.setRecipient(javax.mail.Message.RecipientType.TO, new javax.mail.internet.InternetAddress(notification.getUserEmail()));
+            mail.setSubject(notification.getSubject());
+            mail.setText(notification.getMessage());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        try (javax.mail.Transport transport = javaMailSender.getTransport("smtp")) {
+            transport.connect();
+            transport.sendMessage(mail, mail.getAllRecipients());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
