3733738eb407fa39ecb5e0f0cdbcac8e05b20439
kahchun-haha <kahchunlim885@gmail.com>
2023-06-10 19:36:12 +0800

    modified:   pom.xml 	new file:   src/main/java/com/example/registration_login_demo/dto/NotificationDto.java 	modified:   src/main/resources/application.properties

---
+++ src/main/java/com/example/registration_login_demo/dto/NotificationDto.java
@@ -0,0 +1,16 @@
+package com.example.registration_login_demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationDto {
+    private String userEmail;
+    private String subject;
+    private String message;
+}
