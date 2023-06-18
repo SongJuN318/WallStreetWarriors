f3fe01ff7382b817917d6c191681f3cb2394da26
Shawnchee <aykaychee2@gmail.com>
2023-06-18 21:54:24 +0800

    added alert message javascript

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -3,12 +3,11 @@ var myButton = document.getElementById('btn1');
 var myButton2 = document.getElementById('btn2');
 
 myButton.addEventListener('click', function() {
-    window.location.href = '';
+    alert("You have successfully set your profit threshold!")
 });
 
 myButton2.addEventListener('click', function() {
-
-    window.location.href = 'XXXX';
+    alert("You have successfully set your loss threshold!")
 });
 
 });
