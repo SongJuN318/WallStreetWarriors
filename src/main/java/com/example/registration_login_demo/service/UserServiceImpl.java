5420ceb3e8c0dcfff17e36ad8adf1f1ffc332149
kahchun-haha <133477881+kahchun-haha@users.noreply.github.com>
2023-06-18 22:29:04 +0800

    Update UserServiceImpl.java

--- src/main/java/com/example/registration_login_demo/service/UserServiceImpl.java
+++ src/main/java/com/example/registration_login_demo/service/UserServiceImpl.java
@@ -65,8 +65,6 @@ public class UserServiceImpl implements UserService {
         buyUser.setCurrentFund(50000.0);
         buyUser.setPnl(0.0);
         buyUser.setPoint(0.0);
-        buyUser.setThreshold(2000.0);
-        buyUser.setNotiOnOff(1);
         buyUserRepository.save(buyUser);
     }
 
